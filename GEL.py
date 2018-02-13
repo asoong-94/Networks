4d99900db757014acda709c07c26ce1fd9f706cc
Alex Soong <asoong@ucdavis.edu>
2018-02-13 15:21:51 -0800

    just did schedule

--- GEL.py
+++ GEL.py
@@ -5,8 +5,9 @@ class Event():
 	next_event = pointer to next event 
 	prev_event = pointer to previous event
 	"""
-	def __init__(self, time, event_type, next_event, prev_event):
+	def __init__(self, time, packet, event_type, next_event, prev_event):
 		self.time = time 
+		self.packet = packet
 		self.type = event_type 
 		self.next_event = next_event
 		self.prev_event = prev_event
@@ -20,11 +21,18 @@ class GEL():
 	def __init__(self):
 		self.head = None
 
-	def insert():
+	def schedule(self, event_type, time, packet):
+		# create new event 
+		new_event = Event(time, packet, event_type, None, None)
+		# insert into GEL
+		self.insert(new_event)
+		return 
+
+	def insert(self):
 		#insert an event
 		return 
 
-	def remove():
+	def remove(self):
 		# remove the first event
 		return 
 
